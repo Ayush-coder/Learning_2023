9ff5ffc6ae26b1b8b8105c7502cff7f84d5471fe
Ayush-coder <56168884+Ayush-coder@users.noreply.github.com>
2023-06-16 03:41:49 +0530

    Update 1.c

--- module_1/day_3/1.c
+++ module_1/day_3/1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    printf("Enter size: ");
+    scanf("%d",&size);
+    int n=size;
+    int t=0;
+    int l=1;
+    while(size--)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            printf("%d",i);
+        }
+        while(t--)
+        {
+            printf("  ");
+        }
+        for(int i=n;i>=1;i--)
+        {
+            printf("%d",i);
+        }
+        t=l;
+        l++;
+        n--;
+        printf("\n");
+    }
+}
 
