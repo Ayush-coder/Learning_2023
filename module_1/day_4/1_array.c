34a5d9f8e242cf27150383fc3f79bfeaefe52b72
Ayush-coder <56168884+Ayush-coder@users.noreply.github.com>
2023-06-20 01:51:43 +0530

    Update 1_array.c

--- module_1/day_4/1_array.c
+++ module_1/day_4/1_array.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int size;
+    printf("Enter the size: ");
+    scanf("%d",&size);
+    int arr[size];
+    int sum=0;
+    for(int i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+    }
+    double avg=(double)sum/size;
+    printf("Sum: %d\nAverage: %lf",sum,avg);
+}
 
