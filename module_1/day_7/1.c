4df59b1d07d1627a5483eb67d7395c30fa581c50
Ayush-coder <56168884+Ayush-coder@users.noreply.github.com>
2023-06-22 15:10:38 +0530

    Update 1.c

--- module_1/day_7/1.c
+++ module_1/day_7/1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+typedef struct dim
+{
+    float l,w,h;
+}d;
+
+void print(float vol, float sa)
+{
+    printf("Volume of the box is: %.2f\n",vol);
+    printf("Surface area of box is: %.2f\n",sa);
+}
+
+void dimension(d *dim)
+{
+    float vol=(dim->l*dim->w*dim->h);
+    float sa=((2*dim->l*dim->w)+(2*dim->l*dim->h)+(2*dim->h*dim->w));
+    print(vol,sa);
+}
+
+int main()
+{
+    d dim;
+    printf("Enter Length Width and Height\n");
+    scanf("%f%f%f",&dim.l,&dim.w,&dim.h);
+    dimension(&dim);
+    return 0;
+}
